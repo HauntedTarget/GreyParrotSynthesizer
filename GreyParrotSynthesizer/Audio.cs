530ac40e31a5f08ba21fe6d97d42da2b0765f61f
HauntedTarget <75811611+HauntedTarget@users.noreply.github.com>
2023-11-13 19:35:23 -0700

    Got noise working... might do more lol XD

--- GreyParrotSynthesizer/Audio.cs
+++ GreyParrotSynthesizer/Audio.cs
@@ -20,18 +20,66 @@ namespace GreyParrotSynthesizer
         private const int SAMPLE_RATE = 44100;
         private const short BITS_PER_SAMPLE = 16;
 
-        public static void PlaySineSound(float frequency) // WE MIGHT WANT TO ADD AN AMPLITUDE PARAMETER
+        public static void PlaySineSound(float frequency = 200f) // WE MIGHT WANT TO ADD AN AMPLITUDE PARAMETER
         {
 
             // Only plays for like 1 second though.
             short[] wave = new short[SAMPLE_RATE];
             byte[] binaryWave = new byte[SAMPLE_RATE * sizeof(short)]; // 2 * samplerate
-            frequency = 200f;
             for (int i = 0; i < SAMPLE_RATE; i++)
             {
                 // This is a sine wave
                 // Could have a switch statement here for multiple wave types
                 wave[i] = Convert.ToInt16(short.MaxValue * Math.Sin((2 * Math.PI * frequency / SAMPLE_RATE) * i));
+            }
+            Buffer.BlockCopy(wave, 0, binaryWave, 0, wave.Length * sizeof(short));
+            // Can also use FileSteam to write to a file so we can save it for later
+            using (MemoryStream memoryStream = new MemoryStream())
+            using (BinaryWriter binWriter = new BinaryWriter(memoryStream))
+            {
+                short blockAlign = BITS_PER_SAMPLE / 8;
+                int subChunkTwoSize = SAMPLE_RATE * blockAlign;
+                // HEAD CHUNK
+                binWriter.Write(new char[] { 'R', 'I', 'F', 'F' });
+                binWriter.Write(36 + subChunkTwoSize);
+                binWriter.Write(new char[] { 'W', 'A', 'V', 'E', 'f', 'm', 't', ' ' });
+                binWriter.Write(16);
+                binWriter.Write((short)1);
+                binWriter.Write((short)1);
+                binWriter.Write(SAMPLE_RATE);
+                binWriter.Write(SAMPLE_RATE * blockAlign);
+                binWriter.Write(blockAlign);
+                binWriter.Write(BITS_PER_SAMPLE);
+                // DATA CHUNK
+                binWriter.Write(new char[] { 'd', 'a', 't', 'a' });
+                binWriter.Write(subChunkTwoSize);
+                binWriter.Write(binaryWave);
+                // Point the stream pointer to the beginning of the stream
+                memoryStream.Position = 0;
+                new SoundPlayer(memoryStream).Play();
+            }
+
+        }
+
+        public static void PlayNoise(int seed = -1)
+        {
+            Random rnd = new Random();
+
+            if (seed > 0)
+            {
+                rnd = new(seed);
+            }
+
+            short randomValue = 0;
+
+            // Only plays for like 1 second though.
+            short[] wave = new short[SAMPLE_RATE];
+            byte[] binaryWave = new byte[SAMPLE_RATE * sizeof(short)]; // 2 * samplerate
+            for (int i = 0; i < SAMPLE_RATE; i++)
+            {
+                //Noise wave, short.maxvalue is amplitude so negetive is the lower of it
+                randomValue = Convert.ToInt16(rnd.Next(-short.MaxValue, short.MaxValue));
+                wave[i] = randomValue;
             }
             Buffer.BlockCopy(wave, 0, binaryWave, 0, wave.Length * sizeof(short));
             // Can also use FileSteam to write to a file so we can save it for later
--- GreyParrotSynthesizer/MainSythesizer.cs
+++ GreyParrotSynthesizer/MainSythesizer.cs
@@ -1,3 +1,5 @@
+using System.Media;
+
 namespace GreyParrotSynthesizer
 {
     public partial class MainSythesizer : Form
@@ -5,6 +7,18 @@ namespace GreyParrotSynthesizer
         public MainSythesizer()
         {
             InitializeComponent();
+        }
+
+        private void MainSythesizer_KeyDown(object sender, KeyEventArgs e) 
+        {
+            if (e.KeyCode == Keys.E) 
+            {
+                Audio.PlaySineSound(400f);
+            }
+            else if (e.KeyCode == Keys.R)
+            {
+                Audio.PlayNoise();
+            }
         }
     }
 }
