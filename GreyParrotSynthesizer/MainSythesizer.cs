d80f3abbac1398eb5d19f1f0b1b8e8ed910ce222
Michael <114317916+MFunken7@users.noreply.github.com>
2023-11-15 16:21:56 -0700

    got a frquency slider to work along side the skeleton for amplitude just need the new audio class to finish

--- GreyParrotSynthesizer/MainSythesizer.cs
+++ GreyParrotSynthesizer/MainSythesizer.cs
@@ -2,14 +2,46 @@ namespace GreyParrotSynthesizer
 {
     public partial class MainSythesizer : Form
     {
+
+        WaveForms waveForms = WaveForms.Sin;
+        float frequency = 200f;
+        int amplitude;
+
         public MainSythesizer()
         {
             InitializeComponent();
+            WaveFormDropDown_Load();
+        }
+
+        private void WaveFormDropDown_Load()
+        {
+            WaveFormDropDown.DataSource = System.Enum.GetValues(typeof(WaveForms));
         }
 
         private void MainSythesizer_KeyDown(object sender, KeyEventArgs e)
         {
             Audio.PlaySound(440f, (short)1000, Audio.WaveType.SINE);
+        }
+
+        private void WaveFormDropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str = WaveFormDropDown.Items[WaveFormDropDown.SelectedIndex].ToString();
+            waveForms = (WaveForms)Enum.Parse(typeof(WaveForms), str);
+        }
+
+        private void PlaySound_Click(object sender, EventArgs e)
+        {
+            Audio.PlaySineSound(frequency);
+        }
+
+        private void FrequencyBar_Scroll(object sender, EventArgs e)
+        {
+            frequency = FrequencyBar.Value;
+        }
+
+        private void AmplitudeBar_Scroll(object sender, EventArgs e)
+        {
+            amplitude = AmplitudeBar.Value;
         }
     }
 }
